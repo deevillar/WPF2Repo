26bc3a444d07d663651adc647bfdc3028240049a
Dao Villar <daovillar@yahoo.com>
2014-02-25 22:32:44 -0500

    Functions Industry w/ comments

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,13 +1,19 @@
+// Dao Villar 2/25/14 Functions Assignment Industry
+
+
+// variable seniorCitizen of string type for user if the user is senior citizen or not - it has to be y or n
 var seniorCitizen = "";
-
+// rate of interest for the principal in integer
 var rate;
-
+// principal amount of the investment
 var principal="";
-
+// time for principal amount in years
 var time = 0;
-
+// interest on the principal for given years
 var interest;
-
+// result of application
+var result = "The interest for the principal amount of - " + principal + "$s at rate of " + rate + "% for a time period of " + time + " years is " + getInterestforPrincipal() + "$s";
+// function getRateofInterestbyAge - accepts seniorCitizenParam(y/n) and returns rate (15 for y, 10 for n)
 function getRateofInterestbyAge(seniorCitizenParam) {
     if(seniorCitizenParam == "y"){
         rate = 15;
@@ -16,13 +22,13 @@ function getRateofInterestbyAge(seniorCitizenParam) {
     }
     return rate;
 }
-
+// calculates and returns interest
 function getInterestforPrincipal() {
     interest = principal * time * rate;
     interest = interest / 100;
     return interest;
 }
-
+// while loop for prompting and setting value of seniorCitizen
 while ((seniorCitizen != "y" || seniorCitizen != "n") && seniorCitizen == "") {
     seniorCitizen = prompt("Are you a senior citizen? Enter 'y' for yes and 'n' for no");
     if ((seniorCitizen != "y" || seniorCitizen != "n") && seniorCitizen == "") {
@@ -31,17 +37,19 @@ while ((seniorCitizen != "y" || seniorCitizen != "n") && seniorCitizen == "") {
         alert('You are eligible for '+getRateofInterestbyAge(seniorCitizen)+'% rate for interest');
     }
 }
-
+// while loop for prompting and setting value of principal
 while ((principal == "" || principal == 0) && !isNaN(principal)) {
-    principal = prompt("Please enter the principal for the investment");
+    principal = prompt("Please enter the principal for your investment.");
 }
 
 alert("The principal amount is : " + principal+"$s");
-
+// while loop for prompting and setting value of time
 while ((time == "" || time == 0) && !isNaN(time)) {
     time = prompt("Please enter the time for the investment in years - interest will be calculated according to years only");
 }
 
 alert("The time of investment is : " + time + " years");
 
-alert("The interest for principal amount of - " + principal + "$s at rate of " + rate + "% for a time period of " + time + " years is " + getInterestforPrincipal()+"$s");
+alert("The interest for principal amount of - " + principal + "$s at rate of " + rate + "% for a time period of " + time + " years is " + getInterestforPrincipal() + "$s");
+
+console.log("Result of application is : " + result);
